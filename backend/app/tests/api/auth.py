03b062376ca575e64ce02a3844bbce9ed778cced
hima7459 <violet@himaa.me>
2023-04-13 02:09:02 +0800

    added auth test cases

--- backend/app/tests/api/auth.py
+++ backend/app/tests/api/auth.py
@@ -3,27 +3,25 @@ from fastapi.testclient import TestClient
 from fastapi.encoders import jsonable_encoder
 from app import schemas
 
+
 CREATE_URL = "/auth/create"
 GET_USER_URL = "/auth/get"
 LOGIN_URL = "/auth/login"
 
 
 def test_create_valid_user(
-    test_client: TestClient, test_valid_user: schemas.auth.AccountSchema
-) -> None:
+    client: TestClient, test_valid_user: schemas.auth.AccountSchema
+):
     payload = jsonable_encoder(test_valid_user)
-    response = test_client.post(CREATE_URL, json=payload)
+    response = client.post(CREATE_URL, json=payload)
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"user_id": 1, "username": test_valid_user.username}
+    assert response.json() == {"username": test_valid_user.username}
 
 
-def test_login(
-    test_client: TestClient, test_valid_user: schemas.auth.AccountSchema
-) -> None:
+def test_login(client: TestClient, test_valid_user: schemas.auth.AccountSchema):
     payload = jsonable_encoder(test_valid_user)
-    response = test_client.post(CREATE_URL, json=payload)
-    assert response.status_code == status.HTTP_200_OK
-    response = test_client.post(LOGIN_URL, json=payload)
+    response = client.post(LOGIN_URL, json=payload)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["data"]["username"] == test_valid_user.username
+
